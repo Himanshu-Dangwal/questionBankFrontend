25f2d722ee4b3ceda19b1be320fd70584320670f
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-01-31 10:09:33 +0530

    Updated route to handle fetching questions

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // Change this as per your backend URL
+const API_BASE_URL = "http://localhost:8080/api"; // Change this as per your backend URL
 
 export const login = async (username, password) => {
     try {
@@ -13,7 +13,7 @@ export const login = async (username, password) => {
 
 export const fetchQuestions = async (page) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/questions?page=${page}&limit=20`);
+        const response = await axios.get(`${API_BASE_URL}/dashboard?page=${page}&limit=20`);
         return response.data;
     } catch (error) {
         throw error.response?.data?.message || "Failed to fetch questions";
